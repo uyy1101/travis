f249bca8385d20dea3f6014ef4961b93c5d0e2e4
yolo yu <me@yoloyi.com>
2019-05-08 22:37:01 +0800

    init

---
+++ tests/EmailTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Travis\Test;
+
+use PHPUnit\Framework\TestCase;
+use Travis\Email;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
